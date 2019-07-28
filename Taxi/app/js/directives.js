0da721738e62301c810597ab06f2b6747e1cb6c8
gaizka arizmendiarrieta marina <gaizka_arizmen@hotmail.com>
2019-07-28 20:47:17 +0200

    More changes

--- Taxi/app/js/directives.js
+++ Taxi/app/js/directives.js
@@ -12,9 +12,9 @@ function pageTitle($rootScope, $timeout) {
         link: function(scope, element) {
             var listener = function(event, toState, toParams, fromState, fromParams) {
                 // Default title - load on Dashboard 1
-                var title = 'Antolin | Monitorizacion inyectora';
+                var title = 'NYC TLC | Monitorizacion inyectora';
                 // Create your own title pattern
-                if (toState.data && toState.data.pageTitle) title = 'Antolin | ' + toState.data.pageTitle;
+                if (toState.data && toState.data.pageTitle) title = 'NYC TLC | ' + toState.data.pageTitle;
                 $timeout(function() {
                     element.text(title);
                 });
